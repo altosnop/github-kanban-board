12793c0b1f91fbd290eb197a73c547b5d0037942
Oleksii Larchenko <oleksii.larchen@gmail.com>
2023-04-24 19:50:32 +0300

    Change IssueCard component

--- src/components/IssueCard/index.tsx
+++ src/components/IssueCard/index.tsx
@@ -8,6 +8,13 @@ interface IssueCardProps {
 }
 
 const IssueCard = ({ issue, provided }: IssueCardProps) => {
+	const newDate: Date = new Date();
+	const issueDate: Date = new Date(issue.created_at);
+
+	const diffInDays = Math.ceil(
+		Math.abs(newDate.getTime() - issueDate.getTime()) / (1000 * 3600 * 24)
+	);
+
 	return (
 		<Card
 			key={issue.id}
@@ -19,7 +26,8 @@ const IssueCard = ({ issue, provided }: IssueCardProps) => {
 			{...provided.dragHandleProps}
 			{...provided.draggableProps}
 		>
-			#{issue.number} opened {new Date(issue.created_at).toLocaleString()}{' '}
+			#{issue.number} opened {diffInDays}
+			{diffInDays === 1 ? ` day ago` : ` days ago`}
 			<br />
 			{issue.user.login} | Comments: {issue.comments}
 		</Card>
